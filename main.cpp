24510174915916bf7718c07350163bc36c466bdb
zade shahin <83564652+zadei@users.noreply.github.com>
2022-03-30 16:23:00 +1030

    Added truckloads files

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include <string>
 
 int main() {
+    //Testing for Reverse.cpp
     Reverse reverse;
     std::cout << reverse.reverseDigit(123) << std::endl;
     std::cout << reverse.reverseString("Hello World") << std::endl;
     return 0;
+    
+    //Testing for Truck.cpp
 }
